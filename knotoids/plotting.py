c598e04b638f7d83371aef4d480fa0c68482be98
nyerolemou <nyerolemou@gmail.com>
2023-08-31 09:57:04 +0100

    Classification via Knoto-ID

--- knotoids/plotting.py
+++ knotoids/plotting.py
@@ -11,9 +11,13 @@ def plot_spherical_regions(
     regions: List[Region], curve: Optional[np.ndarray] = None
 ) -> None:
     fig = go.Figure()
-    colours = px.colors.qualitative.Dark24
+    colours = px.colors.qualitative.Light24
+    colour_mapping = {
+        knotoid_class: colours[idx % len(colours)]
+        for idx, knotoid_class in enumerate(set(r.knotoid_class for r in regions))
+    }
     for idx, region in enumerate(regions):
-        colour = colours[idx % 24]
+        colour = colour_mapping[region.knotoid_class]
         points = np.array(
             [node.position for node in region.boundary_nodes]
             + [region.boundary_nodes[0].position]
@@ -41,7 +45,9 @@ def plot_spherical_regions(
         #         y=interpolated[:, 1],
         #         z=interpolated[:, 2],
         #         opacity=0.5,
-        #         color=colour,  # or any color you prefer
+        #         color=colour,
+        #         hoverinfo="text",
+        #         hovertemplate=region.knotoid_class.value,
         #     )
         # )
         # plot representative point
@@ -56,7 +62,9 @@ def plot_spherical_regions(
                 ),
                 mode="markers",
                 hoverinfo="text",
-                hovertemplate=idx,
+                hovertemplate=str(region.knotoid_class.value)
+                + "<br>"
+                + f"{region.area:.3f}",
             )
         )
     if curve is not None:
@@ -73,6 +81,140 @@ def plot_spherical_regions(
                 line=dict(color="black", width=2),
             )
         )
+    fig.update_layout(
+        dict(
+            updatemenus=[
+                dict(
+                    type="buttons",
+                    direction="left",
+                    buttons=list(
+                        [
+                            dict(
+                                args=["visible", "legendonly"],
+                                label="Deselect All",
+                                method="restyle",
+                            ),
+                            dict(
+                                args=["visible", True],
+                                label="Select All",
+                                method="restyle",
+                            ),
+                        ]
+                    ),
+                    pad={"r": 10, "t": 10},
+                    showactive=False,
+                    x=1,
+                    xanchor="right",
+                    y=1.1,
+                    yanchor="top",
+                ),
+            ]
+        )
+    )
+    fig.show()
+
+
+def plot_planar_regions(regions: List[Region]) -> None:
+    fig = go.Figure()
+    colours = px.colors.qualitative.Light24
+    colour_mapping = {
+        knotoid_class: colours[idx % len(colours)]
+        for idx, knotoid_class in enumerate(set(r.knotoid_class for r in regions))
+    }
+    for region in regions:
+        colour = colour_mapping[region.knotoid_class]
+        points = np.asarray(
+            [node.position for node in region.boundary_nodes]
+            + [region.boundary_nodes[0].position]
+        )
+        x, y = points[:, 0] / (1 - points[:, 2]), points[:, 1] / (1 - points[:, 2])
+        if region.is_external:
+            # Calculate bounding box
+            min_x, min_y = np.min(x), np.min(y)
+            max_x, max_y = np.max(x), np.max(y)
+            # Extend the bounding box a little
+            extension = 0.8
+            min_x -= extension
+            min_y -= extension
+            max_x += extension
+            max_y += extension
+            # Bounding box coordinates
+            bounding_x = [min_x, max_x, max_x, min_x, min_x]
+            bounding_y = [min_y, min_y, max_y, max_y, min_y]
+            fill_x = np.concatenate([x, bounding_x])
+            fill_y = np.concatenate([y, bounding_y])
+            fig.add_trace(
+                go.Scatter(
+                    x=[min_x + 0.3],
+                    y=[min_y + 0.3],
+                    marker=dict(
+                        size=3,
+                        color=colour,
+                    ),
+                    line=dict(color=colour, width=2),
+                    mode="markers",
+                    hoverinfo="text",
+                    hovertemplate=region.knotoid_class.value,
+                )
+            )
+            fig.add_trace(
+                go.Scatter(
+                    x=x,
+                    y=y,
+                    mode="lines",
+                    line=dict(width=2, color=colour),
+                )
+            )
+            fig.add_trace(
+                go.Scatter(
+                    x=bounding_x,
+                    y=bounding_y,
+                    mode="lines",
+                    line=dict(width=2, color=colour),
+                )
+            )
+            fig.add_trace(
+                go.Scatter(
+                    x=fill_x,
+                    y=fill_y,
+                    fill="toself",
+                    # fillcolor=colour,
+                    line=dict(color=colour, width=0),
+                    hoverinfo="text",
+                    hovertemplate=region.knotoid_class.value,
+                )
+            )
+        else:
+            fig.add_trace(
+                go.Scatter(
+                    x=x,
+                    y=y,
+                    marker=dict(
+                        size=1,
+                        color=colour,
+                    ),
+                    line=dict(color=colour, width=2),
+                    fill="toself",
+                    hoveron="points+fills",
+                    hoverinfo="text",
+                    hovertemplate=region.knotoid_class.value,
+                )
+            )
+            fig.add_trace(
+                go.Scatter(
+                    x=[region.internal_point[0] / (1 - region.internal_point[2])],
+                    y=[region.internal_point[1] / (1 - region.internal_point[2])],
+                    marker=dict(
+                        size=3,
+                        color=colour,
+                    ),
+                    line=dict(color=colour, width=2),
+                    mode="markers",
+                    hoveron="points+fills",
+                    hoverinfo="text",
+                    hovertemplate=region.knotoid_class.value,
+                )
+            )
     fig.update_layout(
         dict(
             updatemenus=[
